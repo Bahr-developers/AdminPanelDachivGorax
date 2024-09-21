30bd5d0264abfe3ba93ae5cb65fe727421a2104a
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-09-21 18:27:43 +0500

    edw

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,13 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
+import { SERVER_BASE_URL } from "./constants/server.constants.js";
 
 const queryClient = new QueryClient();
+
+
+console.log(SERVER_BASE_URL);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
